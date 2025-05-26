cabe5eff410e7ae56dcfea38e2b5151365fd4c38
harshad8180 <hpborkar@gmail.com>
2025-05-26 19:50:21 +0530

    user entity created

---
+++ ecommerce/src/main/java/com/ecom/entity/User.java
@@ -0,0 +1,32 @@
+package com.ecom.entity;
+
+import com.ecom.domain.USER_ROLE;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private String email;
+
+    private String fullName;
+
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+
+    private Set<Address> addresses = new HashSet<>();
+
+    private Set<Coupon> usedCoupons = new HashSet<>();
+}
