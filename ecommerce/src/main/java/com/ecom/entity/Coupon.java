b5371a871fbc811541b76db830dc60ed1e9028fa
harshad8180 <hpborkar@gmail.com>
2025-05-26 19:57:56 +0530

    created address and coupon entity

--- ecommerce/src/main/java/com/ecom/entity/Coupon.java
+++ ecommerce/src/main/java/com/ecom/entity/Coupon.java
@@ -1,4 +1,22 @@
 package com.ecom.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
--- ecommerce/src/main/java/com/ecom/entity/User.java
+++ ecommerce/src/main/java/com/ecom/entity/User.java
@@ -6,11 +6,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class User {
 
     @Id
