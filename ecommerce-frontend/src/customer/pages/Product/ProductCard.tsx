bfb558b45ce8d18c0760f1ac28b56394b9850a07
harshad8180 <hpborkar@gmail.com>
2025-06-09 18:24:08 +0530

    added hivering effect to each product give dummy image array to see ui and below pricing

--- ecommerce-frontend/src/customer/pages/Product/ProductCard.tsx
+++ ecommerce-frontend/src/customer/pages/Product/ProductCard.tsx
@@ -40,7 +40,7 @@ const ProductCard = () => {
         }
 
         {
-            <div className='indicator flex flex-col items-center space-y-2'>
+            isHovered && <div className='indicator flex flex-col items-center space-y-2'>
                 <div className="flex gap-3">
                     <Button variant='contained' color='secondary'>
                         <Favorite sx={{color:teal[500]}}/>
@@ -50,10 +50,34 @@ const ProductCard = () => {
                         <ModeComment sx={{color:teal[500]}}/>
                     </Button>
                 </div>
+
+                
             </div>
 
         }
       </div>
+
+        <div className="details pt-3 space-y-1 group-hover-effect rounded-md">
+                    <div className="name">
+                        <h1>Niky</h1>
+                        <p>Blue T-Shirt</p>
+                    </div>
+
+                    <div className="price flex items-center gap3">
+                        <span className="font-sans text-gray-800">
+                            ₹ 400
+                        </span>
+
+                        <span className="thin-line-through text-gray-400">
+                            ₹ 999
+                        </span>
+
+                        <span className="text-primary-color font-semibold">
+                            60% off
+                        </span>
+                    </div>
+                </div>
+
     </div>
   )
 }
