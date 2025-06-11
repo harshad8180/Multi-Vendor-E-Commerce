4062822c69c3593908319ae29268e7b41da7ed7d
harshad8180 <hpborkar@gmail.com>
2025-06-11 13:19:25 +0530

    apply coupon ui implemented

--- ecommerce-frontend/src/customer/pages/Cart/CartItem.tsx
+++ ecommerce-frontend/src/customer/pages/Cart/CartItem.tsx
@@ -3,65 +3,65 @@ import { Button, Divider, IconButton } from '@mui/material'
 import React from 'react'
 
 const CartItem = () => {
-    const handleUpdateQuantity = () =>{
+    const handleUpdateQuantity = () => {
 
     }
-  return (
-    <div className='border rounded-md relative'>
-        <div className="p-5 flex gap-">
-            <div>
-                <img className='rounded-md w-[90px]' src="https://m.media-amazon.com/images/I/71ftvs1yo1L._AC_AA300_.jpg" alt="" />
+    return (
+        <div className='border rounded-md relative'>
+            <div className="p-5 flex gap-">
+                <div>
+                    <img className='rounded-md w-[90px]' src="https://m.media-amazon.com/images/I/71ftvs1yo1L._AC_AA300_.jpg" alt="" />
+                </div>
+
+                <div className="space-y-2">
+                    <h1 className='font-semibold text-lg'>Soch Clothing</h1>
+
+                    <p className='text-gray-600 font-medium text-sm'><strong> Care instructions:</strong> Hand Wash Only,
+                        The saree is in purple color and net fabric.</p>
+                    <p>Saree Length - <strong>5.5 Meter</strong></p>
+                    <p>Blouse Length - <strong>0.8 Meter</strong></p>
+
+
+                    <p className='text-xs'>In Box: Saree with unstitched blouse</p>
+                    <p className='text-gray-400 text-xs'> <strong>Sold by : </strong> SOCH APPARELS PVT LTD</p>
+                    <p className='text-sm text-gray-500'><strong>quantity : </strong> 5 </p>
+                </div>
             </div>
-
-            <div className="space-y-2">
-                <h1 className='font-semibold text-lg'>Soch Clothing</h1>
-
-                <p className='text-gray-600 font-medium text-sm'><strong> Care instructions:</strong> Hand Wash Only,
-The saree is in purple color and net fabric.</p>
-<p>Saree Length - <strong>5.5 Meter</strong></p>
-<p>Blouse Length - <strong>0.8 Meter</strong></p>
-
-
-<p className='text-gray-400 text-xs'>In Box: Saree with unstitched blouse</p>
-<p className='text-sm'> <strong>Sold by : </strong> SOCH APPARELS PVT LTD</p>              
-<p className='text-sm text-gray-500'><strong>quantity : </strong> 5 </p>
-            </div>
-        </div>      
-            <Divider/>
+            <Divider />
 
             <div className='flex justify-between items-center'>
-            <div className='px-5 py-2 flex justify-between items-center'>
+                <div className='px-5 py-2 flex justify-between items-center'>
 
-                <div className='flex items-center gap-2 w-[140px] justify-between'>
+                    <div className='flex items-center gap-2 w-[140px] justify-between'>
 
-                    <Button onClick={handleUpdateQuantity} disabled={true}>
-                        <Remove/>
-                    </Button>
+                        <Button onClick={handleUpdateQuantity} disabled={true}>
+                            <Remove />
+                        </Button>
 
-                    <span>{5}</span>
+                        <span>{5}</span>
 
-                    <Button onClick={handleUpdateQuantity}>
-                        <Add/>
-                    </Button>
+                        <Button onClick={handleUpdateQuantity}>
+                            <Add />
+                        </Button>
+
+                    </div>
 
                 </div>
 
-            </div>
-
-            <div className='pr-5'>
-                <p className='text-gray-700 font-medium'>₹799</p>
-            </div>
+                <div className='pr-5'>
+                    <p className='text-gray-700 font-medium'>₹799</p>
+                </div>
 
             </div>
 
             <div className="absolute top-1 right-1 ">
                 <IconButton color='primary'>
-                    <Close/>
+                    <Close />
                 </IconButton>
             </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default CartItem
