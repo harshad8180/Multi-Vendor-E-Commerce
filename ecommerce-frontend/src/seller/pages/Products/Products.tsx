4c27eb2063ce5c103bbaeda72e901401435ab05f
harshad8180 <hpborkar@gmail.com>
2025-06-28 21:55:51 +0530

    mui table for product

--- ecommerce-frontend/src/seller/pages/Products/Products.tsx
+++ ecommerce-frontend/src/seller/pages/Products/Products.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProductTable from './ProductTable'
 
 const Products = () => {
   return (
     <div>
-      
+      <h1 className="font-bold mb-5 text-xl ">All Products</h1>
+      <ProductTable/>
     </div>
   )
 }
