6038b4006a839f38280c2ec584a934db302d6d21
harshad8180 <hpborkar@gmail.com>
2025-06-28 22:13:43 +0530

    mui table for payment

--- ecommerce-frontend/src/seller/pages/Payment/Payment.tsx
+++ ecommerce-frontend/src/seller/pages/Payment/Payment.tsx
@@ -1,9 +1,25 @@
+import { Button, Card, Divider } from '@mui/material'
 import React from 'react'
+import Transaction from './Transaction'
 
 const Payment = () => {
   return (
-    <div>
-      
+    <div className='space-y-5'>
+      <Card className='rounded-md space-y-4 p-5'>
+        <h1 className='text-gray-600 font-medium'>Total Earning</h1>
+
+        <h2 className='font-bold text-xl pb-1'>₹19876</h2>
+        <Divider/>
+
+        <p className='text-gray-600 font-medium pt-1'>Last Payment : <strong>₹0</strong> </p>
+      </Card>
+
+      <div className='pt-20 space-y-3'>
+      <Button variant='contained'>
+        Transaction
+      </Button>
+      <Transaction/>
+      </div>
     </div>
   )
 }
